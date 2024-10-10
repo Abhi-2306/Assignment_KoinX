551516512dc688a0723dd0372f190120b8e84550
Abhi-2306 <abhijtih.pandiri@gmail.com>
2024-10-11 00:09:34 +0530

    task-3 done

--- controllers/cryptoController.js
+++ controllers/cryptoController.js
@@ -1,5 +1,28 @@
 const { getData } = require("../fetch_data");
 const crypto = require("../models/crypto");
+
+exports.getStandardDeviation = async (req, res) => {
+    try {
+        const { coin } = req.query;
+        if (!coin || !["bitcoin", "ethereum", "matic-network"].includes(coin)) {
+            return res.status(400).send("Invalid coin");
+        }
+        const data = await crypto.find({ name: coin }).sort({ createdAt: -1 }).limit(100);
+
+        if (data.length === 0) {
+            res.status(404).json({ message: "Data not found for this coin" });
+        }
+
+        const prices = data.map(item => item.price);
+        const mean = prices.reduce((a, b) => a + b, 0) / prices.length;
+        const squared = prices.map((price) => (price - mean) ** 2);
+        const variance = squared.reduce((a, b) => a + b, 0) / prices.length;
+        const deviation = Math.sqrt(variance);
+        res.status(200).json({ deviation });
+    } catch (error) {
+        res.status(500).send("Error fetching deviation");
+    }
+}
 exports.getCryptoLatestData = async (req, res) => {
     try {
         const { coin } = req.query;
--- routes/cryptoRoutes.js
+++ routes/cryptoRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {getCryptoLatestData} = require("../controllers/cryptoController");
+const {getCryptoLatestData, getStandardDeviation} = require("../controllers/cryptoController");
 
 router.get("/stats", getCryptoLatestData);
+router.get("/deviation",getStandardDeviation);
 
 module.exports = router;
