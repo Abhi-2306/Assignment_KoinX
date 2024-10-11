b45dab627781c019ff0ed81c5529073c8e96b8eb
Abhi-2306 <abhijtih.pandiri@gmail.com>
2024-10-11 12:25:08 +0530

    added workflows

--- server.js
+++ server.js
@@ -14,10 +14,16 @@ const PORT = process.env.PORT || 5000;
 app.get("/", async (req, res) => {
     res.send("Server is running <br> Add the following in the URL:<br> /api/stats?coin=(bitcoin or ethereum or matic-network) to get latest stats<br> /api/deviation?coin=(bitcoin or ethereum or matic-network) to get deviation stats");
 });
-cron.schedule("0 */2 * * *", async () => {
-    await fetchAndSaveCryptoData();
-    console.log("Data fetched and saved");
-})
+app.post("/api/fetchData", async (req, res) => {
+    try {
+        await fetchAndSaveCryptoData();
+        console.log("Data fetched and saved");
+        return res.status(200).json({ message: "Data fetched and saved successfully." });
+    } catch (error) {
+        console.error("Error fetching and saving data:", error);
+        return res.status(500).json({ message: "Error fetching data." });
+    }
+});
 app.use("/api", cryptoRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
